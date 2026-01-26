b314018070486c7a66aef9c34804ebd780e90c7f
mwesigye john bosco <johnboscomwesigye@gmail.com>
2026-01-26 22:50:37 +0300

    Fix Sms Event Page (#20)

--- src/app/system/sms-event-configuration/sms-event-configuration.component.ts
+++ src/app/system/sms-event-configuration/sms-event-configuration.component.ts
@@ -1,7 +1,19 @@
 import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, MatSortHeader } from '@angular/material/sort';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+  MatTableDataSource
+} from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { SystemService } from '../system.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,23 +21,47 @@ import { MatLabel } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { TranslateModule } from '@ngx-translate/core';
+import { HasPermissionDirective } from 'app/directives/has-permission/has-permission.directive';
+import { TranslatePipe } from '@pipes/translate.pipe';
+import { CommonModule } from '@angular/common';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import { STANDALONE_SHARED_IMPORTS } from 'app/standalone-shared.module';
 
 @Component({
   selector: 'mifosx-sms-event-configuration',
   templateUrl: './sms-event-configuration.component.html',
   styleUrls: ['./sms-event-configuration.component.scss'],
-  standalone: true,
   imports: [
+    ...STANDALONE_SHARED_IMPORTS,
+    CommonModule,
     MatPaginator,
     MatSort,
     MatFormFieldModule,
     MatLabel,
     MatTableModule,
     MatInputModule,
-    TranslateModule
+    TranslateModule,
+    HasPermissionDirective,
+    TranslatePipe,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCellDef,
+    MatHeaderCell,
+    MatSortHeader,
+    MatCellDef,
+    MatCell,
+    MatSlideToggle,
+    FormsModule,
+    MatHeaderRowDef,
+    MatHeaderRow,
+    MatRowDef,
+    MatRow
   ]
 })
 export class SmsEventConfigurationComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+  private systemService = inject(SystemService);
   /** Events Data. */
   eventsData: any;
   smsEventConfigurations: any = {};
@@ -45,8 +81,6 @@ export class SmsEventConfigurationComponent implements OnInit {
   /** Sorter for reports table. */
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  private route = inject(ActivatedRoute);
-  private systemService = inject(SystemService);
 
   constructor() {
     this.route.data.subscribe((data: { events: any }) => {
@@ -71,7 +105,7 @@ export class SmsEventConfigurationComponent implements OnInit {
    * Enables/Disables respective event
    */
   toggleStatus(event: any) {
-    this.smsEventConfigurations[event.type] = !event.enabled;
+    this.smsEventConfigurations[event.type] = event.enabled;
     this.existAnyUpdate = true;
   }
 

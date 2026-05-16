2b357d7865994a05e443be10059b055990fcc284
mwesigye john bosco <johnboscomwesigye@gmail.com>
2026-05-16 12:36:35 +0300

    RPAY-49 :- Hide sms cost from transaction table (#25)

--- src/app/system/premium-feature/sms/sms-wallet-transactions.component.ts
+++ src/app/system/premium-feature/sms/sms-wallet-transactions.component.ts
@@ -60,7 +60,6 @@ export class SmsWalletTransactionsComponent implements OnInit {
   displayedColumns: string[] = [
     'id',
     'amount',
-    'smsCost',
     'smsCredit',
     'previousBalance',
     'note',
